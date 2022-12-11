8249e3390679ed976f3bd8aed10ef72bdb71614d
kirill <leontev.kirill@phystech.edu>
2022-12-11 20:11:43 +0300

    some changes

--- project/main.cpp
+++ project/main.cpp
@@ -4,31 +4,39 @@
 #include <iostream>
 template <typename T>
 class Imaginary{
-private:
-    T* val; // val[0] - real part, val[1] - Imaginary part
 public:
-    Imaginary():val(new T[2]){}
+    T* _val; // _val[0] - real part, _val[1] - Imaginary part
+    Imaginary():_val(new T[2]){}
     Imaginary(T re, T im): Imaginary(){
-        val[0] = re;
-        val[1] = im;
+        _val[0] = re;
+        _val[1] = im;
+    }
+    /// copy constructor
+    Imaginary(Imaginary<T>& g): Imaginary(){
+        _val[0] = g.Re();
+        _val[1] = g.Im();
+    }
+    /// move constructor
+    Imaginary(Imaginary<T>&& g): Imaginary(){
+        // TODO how to make it?
     }
     /// imaginary unit
     Imaginary(char a):Imaginary(){
-        val[0] = 0;
-        val[1] = 1;
+        _val[0] = 0;
+        _val[1] = 1;
     }
     /// conversion from reals
     Imaginary(T a): Imaginary(){
-        val[0] = a;
-        val[1] = 0;
+        _val[0] = a;
+        _val[1] = 0;
     }
     /// copy assignment operator
     Imaginary<T>& operator =(Imaginary<T>& g){
-        T* tmp_val = new T[2];
-        tmp_val[0] = g.Re();
-        tmp_val[1] = g.Im();
-        delete [] val;
-        val= tmp_val;
+        T* tmp__val = new T[2];
+        tmp__val[0] = g.Re();
+        tmp__val[1] = g.Im();
+        delete [] _val;
+        _val= tmp__val;
         return *this;
     }
     /// copy assignment operator from reals
@@ -36,20 +44,22 @@ public:
         T* tmp_val = new T[2];
         tmp_val[0] = g;
         tmp_val[1] = 0;
-        delete [] val;
-        val= tmp_val;
+        delete [] _val;
+        _val= tmp_val;
         return *this;
     }
     /// move assignment operator
     Imaginary<T>& operator =(Imaginary<T>&& g){
-
+        /*Imaginary<T> tmp(std::move(g)); // TODO how to make it?
+        std::swap(this->_val, tmp.data);*/
+        return *this;
     }
     /// move assignment operator from reals
     Imaginary<T>& operator =(T&& g){
-
+        // TODO how to make it?
     }
     ~Imaginary(){
-        delete[] val;
+        delete[] _val;
     }
     /// each operator also for reals
     ///plus
@@ -74,36 +84,34 @@ public:
     Imaginary<T>& operator -(){}
     /// get real part
     T Re(){
-        return val[0];
+        return _val[0];
     }
     /// get imaginary part
     T Im(){
-        return val[1];
+        return _val[1];
     }
     /// module squared |a|^2
     T ModuleSq(){
-        return val[0]*val[0]+val[1]*val[1];
+        return _val[0]*_val[0]+_val[1]*_val[1];
     }
     /// conjurate of a: a - b*i
     Imaginary<T>& Conj(){
-        return Imaginary<T>(val[0], -val[1]);
+        return Imaginary<T>(_val[0], -_val[1]);
     }
 };
 template <typename T>
 class Quaternion{
-private:
-    T* val;/* val[0] - real part,
-                 val[1] - Imaginary part
-                 val[2] - j-part
-                 val[3] - k-part*/
-
 public:
-    Quaternion():val(new T[4]){}
+    T* _val;/* _val[0] - real part,
+                 _val[1] - Imaginary part
+                 _val[2] - j-part
+                 _val[3] - k-part*/
+    Quaternion():_val(new T[4]){}
     Quaternion(T re, T i, T j, T k): Quaternion(){
-        val[0] = re;
-        val[1] = i;
-        val[2] = j;
-        val[3] = k;
+        _val[0] = re;
+        _val[1] = i;
+        _val[2] = j;
+        _val[3] = k;
     }
     /// basic quaternion units: "i", "j", "k"
     Quaternion(char a): Quaternion(){
@@ -138,7 +146,7 @@ public:
 
     }
     ~Quaternion(){
-        delete[] val;
+        delete[] _val;
     }
     /// each operator also for reals and imaginary
     ///plus
@@ -171,27 +179,27 @@ public:
     Quaternion<T> & operator -(){}
     /// get real part
     T Re(){
-        return val[0];
+        return _val[0];
     }
     /// get Imaginary part
     T Im(){
-        return val[1];
+        return _val[1];
     }
     /// get j-part
     T j(){
-        return val[2];
+        return _val[2];
     }
     /// get k-part part
     T k(){
-        return val[3];
+        return _val[3];
     }
     /// module squared |a|^2
     T ModuleSq(){
-        return val[0]*val[0]+val[1]*val[1]+val[2]*val[2]+val[3]*val[3];
+        return _val[0]*_val[0]+_val[1]*_val[1]+_val[2]*_val[2]+_val[3]*_val[3];
     }
     /// conjurate of a: a - b*i - c*j-d*k
     Quaternion<T>& Conj(){
-        return Quaternion<T>(val[0], -val[1], -val[2], -val[3]);
+        return Quaternion<T>(_val[0], -_val[1], -_val[2], -_val[3]);
     }
 };
 int main(){
